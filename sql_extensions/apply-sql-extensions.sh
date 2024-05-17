2c3a1511cc00056891e2fec44d78c55fa701662b
Bogdan Crisan <bogdan@crbl.studio>
2024-05-17 19:12:46 +0200

    [ECO-1681] Use docker hub (#7)
    * add caching in gh ci
    
    * add github ci
    
    * add word to dictionary

--- sql_extensions/apply-sql-extensions.sh
+++ sql_extensions/apply-sql-extensions.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-for file in $(ls migrations/*.sql);do
-    if [ "$(psql $DATABASE_URL --csv -t -c "SELECT COUNT(*) FROM sql_extensions WHERE name = '$file'" 2>/dev/null)" != "1" ];then
-        psql $DATABASE_URL -c "\i $file"
-        psql $DATABASE_URL -c "INSERT INTO sql_extensions VALUES ('$file')"
-    fi
-done
+sleep 1
+
+if [ -d "migrations" ]; then
+    for file in 00000_init.sql $(ls migrations/*.sql);do
+        if [ "$(psql $DATABASE_URL --csv -t -c "SELECT COUNT(*) FROM sql_extensions WHERE name = '$file'" 2>/dev/null)" != "1" ];then
+            psql $DATABASE_URL --single-transaction -f "$file" -c "INSERT INTO sql_extensions VALUES ('$file');"
+        fi
+    done
+fi
