d7b159d51682a1e7cd4999fe84252b860823bd05
Bogdan Crisan <bogdan@crbl.studio>
2024-05-21 02:08:46 +0200

    add terraform support

---
+++ terraform/modules/mqtt/main.tf
@@ -0,0 +1,39 @@
+resource "terraform_data" "instance" {
+  # Store zone since variables not accessible at destroy time.
+  input = var.zone
+  provisioner "local-exec" {
+    command = join(" ", [
+      "gcloud compute instances create-with-container mqtt",
+      "--container-env",
+      join(",", [
+        "MQTT_PASSWORD=${var.mosquitto_password}",
+        "DATABASE_URL=${var.db_conn_str_private}"
+      ]),
+      "--container-image econialabs/inbox:mqtt",
+      "--network ${var.sql_network_id}",
+      "--zone ${var.zone}"
+    ])
+  }
+  provisioner "local-exec" {
+    command = join("\n", [
+      "result=$(gcloud compute instances list --filter NAME=mqtt)",
+      "if [ -n \"$result\" ]; then",
+      join(" ", [
+        "gcloud compute instances delete mqtt",
+        "--quiet",
+        "--zone ${self.output}"
+      ]),
+      "fi"
+    ])
+    when = destroy
+  }
+}
+
+data "external" "ip" {
+  depends_on = [terraform_data.instance]
+  program = [
+    "bash",
+    "-c",
+    "gcloud compute instances list --filter name=mqtt --format 'json(networkInterfaces[0].accessConfigs[0].natIP)' | jq '.[0].networkInterfaces[0].accessConfigs[0]'",
+  ]
+}
---
+++ terraform/modules/processor/variables.tf
@@ -0,0 +1,15 @@
+variable "db_conn_str_private" {}
+
+variable "contract_address" {}
+
+variable "grpc_auth_token" {}
+
+variable "grpc_data_service_url" {}
+
+variable "migrations_complete" {}
+
+variable "starting_version" {}
+
+variable "zone" {}
+
+variable "sql_network_id" {}
