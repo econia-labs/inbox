b56bd3187138f93de105106f93ec07e0145bb579
Bogdan Crisan <bogdan@crbl.studio>
2024-05-30 11:33:53 +0200

    add mqtt events for emojicoin events

---
+++ sql_extensions/migrations/mqtt-events.sql
@@ -0,0 +1,67 @@
+CREATE OR REPLACE FUNCTION notify_periodic_event()
+  RETURNS trigger AS $$
+DECLARE
+BEGIN
+  PERFORM pg_notify(
+    'inbox_event',
+    (SELECT jsonb_build_object('topic', (NEW.type || '/' || (((NEW.data::jsonb)->('market_metadata'::text))::jsonb->>('market_id'::text)) || '/' || ((NEW.data::jsonb->('periodic_state_metadata'::text))::jsonb->>('period'::text))), 'payload', to_jsonb(NEW))::text));
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER notify_periodic_event
+  AFTER INSERT ON inbox_events
+  FOR EACH ROW
+  WHEN (NEW.data ? 'market_metadata' AND NEW.data ? 'periodic_state_metadata')
+  EXECUTE PROCEDURE notify_periodic_event();
+
+CREATE OR REPLACE FUNCTION notify_metadata_event()
+  RETURNS trigger AS $$
+DECLARE
+BEGIN
+  PERFORM pg_notify(
+    'inbox_event',
+    (SELECT jsonb_build_object('topic', (NEW.type || '/' || (((NEW.data::jsonb)->('market_metadata'::text))::jsonb->>('market_id'::text))), 'payload', to_jsonb(NEW))::text));
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER notify_metadata_event
+  AFTER INSERT ON inbox_events
+  FOR EACH ROW
+  WHEN (NEW.data ? 'market_metadata' AND NOT NEW.data ? 'periodic_state_metadata')
+  EXECUTE PROCEDURE notify_metadata_event();
+
+CREATE OR REPLACE FUNCTION notify_flat_event()
+  RETURNS trigger AS $$
+DECLARE
+BEGIN
+  PERFORM pg_notify(
+    'inbox_event',
+    (SELECT jsonb_build_object('topic', (NEW.type || '/' || ((NEW.data::jsonb)->>('market_id'::text))), 'payload', to_jsonb(NEW))::text));
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER notify_flat_event
+  AFTER INSERT ON inbox_events
+  FOR EACH ROW
+  WHEN (NEW.data ? 'market_id' AND NOT NEW.data ? 'results_in_state_transition')
+  EXECUTE PROCEDURE notify_flat_event();
+
+CREATE OR REPLACE FUNCTION notify_swap_event()
+  RETURNS trigger AS $$
+DECLARE
+BEGIN
+  PERFORM pg_notify(
+    'inbox_event',
+    (SELECT jsonb_build_object('topic', (NEW.type || '/' || ((NEW.data::jsonb)->>('market_id'::text)) || '/' || ((NEW.data::jsonb)->>('results_in_state_transition'::text))), 'payload', to_jsonb(NEW))::text));
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER notify_swap_event
+  AFTER INSERT ON inbox_events
+  FOR EACH ROW
+  WHEN (NEW.data ? 'market_id' AND NEW.data ? 'results_in_state_transition')
+  EXECUTE PROCEDURE notify_swap_event();
