6e0b71630907f528f78224b0f787f875e1e8acb9
matt <xbtmatt@gmail.com>
2024-06-02 21:12:12 -0700

    Remove schema refresh since I can just do it locally

--- sql_extensions/apply-sql-extensions.sh
+++ sql_extensions/apply-sql-extensions.sh
@@ -12,7 +12,6 @@ if [ -d "$script_dir/migrations" ]; then
             psql $DATABASE_URL --single-transaction -f "$file" -c "INSERT INTO sql_extensions VALUES ('$(basename $file)');"
         fi
     done
-    psql $DATABASE_URL -t -c "NOTIFY pgrst, 'reload schema';"
 fi
 
 echo "Migrations successfully applied."
